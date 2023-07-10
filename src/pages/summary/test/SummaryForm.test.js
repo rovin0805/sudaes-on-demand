7c3db58a9438867924faeb12d73cec0771cbdd4e
Choi Eun Jeong <qhdrn67@naver.com>
2023-07-10 22:59:43 +0900

    fix : firEvent => userEvent

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 test('Initial  conditions', () => {
   render(<SummaryForm />);
@@ -13,7 +14,9 @@ test('Initial  conditions', () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test('Checkbox enables button on first click and  disables on second click', () => {
+test('Checkbox enables button on first click and  disables on second click', async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole('checkbox', {
@@ -21,9 +24,9 @@ test('Checkbox enables button on first click and  disables on second click', () 
   });
   const confirmButton = screen.getByRole('button', { name: /confirm/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
