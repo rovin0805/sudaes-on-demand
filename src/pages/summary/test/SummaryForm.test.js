efd0ff3f0c6cd3f7ee4bfc36e938a93ea614520f
Choi Eun Jeong <qhdrn67@naver.com>
2023-07-10 22:40:47 +0900

    SummaryForm checkbox & button tests

---
+++ src/pages/summary/test/SummaryForm.test.js
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Initial  conditions', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and  disables on second click', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole('button', { name: /confirm/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
