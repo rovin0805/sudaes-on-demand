04632954d9b2c33ba149cbb4ad8bad59fb99368b
Choi Eun Jeong <qhdrn67@naver.com>
2023-07-14 19:12:05 +0900

    SummaryForm popover test

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -30,3 +30,23 @@ test('Checkbox enables button on first click and  disables on second click', asy
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
