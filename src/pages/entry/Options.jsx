e803c9f5d28305b3723619e7d853bd7d0cdf92e7
Choi Eun Jeong <qhdrn67@naver.com>
2023-07-16 21:52:59 +0900

    Test and code context for scoops subtotal

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,12 +4,16 @@ import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import { Row } from 'react-bootstrap';
 import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
+import { formatCurrency } from '../../utilities';
 
 // optionType : scoops || toppings
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   useEffect(() => {
     axios
@@ -26,6 +30,8 @@ export default function Options({ optionType }) {
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
@@ -34,5 +40,14 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,6 +1,16 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function ScoopOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) =>
+    updateItemCount({
+      itemName: name,
+      newItemCount: +e.target.value,
+      optionType: 'scoops',
+    });
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +18,22 @@ export default function ScoopOption({ name, imagePath }) {
         src={`${process.env.REACT_APP_BASE_URL}${imagePath}`}
         alt={`${name} scoop`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: '10px' }}
+      >
+        <Form.Label column xs='6' style={{ textAlign: 'right' }}>
+          {name}
+        </Form.Label>
+        <Col xs='5' style={{ textAlign: 'left' }}>
+          <Form.Control
+            type='number'
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
